822bb6adcd7dd73efb8d82ad544eb9ea4730f537
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-01-13 09:16:57 +0900

    Update centuryFromYear.py

--- codesignal/centuryFromYear.py
+++ codesignal/centuryFromYear.py
@@ -7,3 +7,7 @@ def centuryFromYear(year):
         return round(year/100)
     else :
         return round(year/100)
+
+# Codesignal>Arcade>Intro #2 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/01/10
+# When I finish all Arcade, I can give a good answer.
