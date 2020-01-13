0a6f4da7663c360b968f9ff239ccae18a923bc3c
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-01-13 14:56:01 +0900

    Update adjacentElementsProduct.py

--- codesignal/intro/adjacentElementsProduct.py
+++ codesignal/intro/adjacentElementsProduct.py
@@ -4,3 +4,7 @@ def adjacentElementsProduct(inputArray):
         k=inputArray[i]*inputArray[i+1]
         b.append(k)
     return max(b)
+
+# Codesignal>Arcade>Intro #4 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/01/13
+# When I finish all Arcade, I can give a good answer.
