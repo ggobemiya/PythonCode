05bcbb7d2f0df5a8778e159c1005a1ec1b9655a7
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-01-14 09:15:19 +0900

    Create makeArrayConsecutive2.py

---
+++ codesignal/intro/makeArrayConsecutive2.py
@@ -0,0 +1,6 @@
+def makeArrayConsecutive2(statues):
+    return max(statues) - min(statues) -len(statues)+1
+    
+# Codesignal>Arcade>Intro #6 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/01/14
+# When I finish all Arcade, I can give a good answer.
