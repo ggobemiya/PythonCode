2b5add9f6330dad57c3d0e82eef5e0d66fadb1e7
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-01-14 09:14:30 +0900

    Create shapeArea.py

---
+++ codesignal/intro/shapeArea.py
@@ -0,0 +1,9 @@
+def shapeArea(n):
+    return 2*n*n-2*n+1
+
+# Mathmatics Problem. arithmetic progression, geometric progression
+# 수학문제. 등차수열, 등비수열
+
+# Codesignal>Arcade>Intro #5 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/01/13
+# When I finish all Arcade, I can give a good answer.
