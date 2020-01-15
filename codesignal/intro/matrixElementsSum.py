ad6d2f11ea56ed522deb8877bf935701bd4e0e01
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-01-15 09:27:16 +0900

    Update matrixElementsSum.py

--- codesignal/intro/matrixElementsSum.py
+++ codesignal/intro/matrixElementsSum.py
@@ -6,3 +6,8 @@ def matrixElementsSum(matrix):
                 break
             k.append(matrix[i][j])
     return sum(k)
+
+
+# Codesignal>Arcade>Intro #8 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/01/15
+# When I finish all Arcade, I can give a good answer.
