fba6cca4848eb506e5f7ee546515675f16543d0c
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-01-17 09:49:25 +0900

    Create alternatingSums.py

---
+++ codesignal/intro/alternatingSums.py
@@ -0,0 +1,8 @@
+def alternatingSums(a):
+    b = a[::2]
+    c = sum(a)-sum(b)
+    return [sum(b),c]
+
+# Codesignal>Arcade>Intro #14 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/01/17
+# When I finish all Arcade, I can give a good answer.
