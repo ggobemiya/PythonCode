acbc43db76003cba9f3f6b069c8ecfb863eed32b
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-02-10 09:52:28 +0900

    Create differentSymbolsNaive.py

---
+++ codesignal/intro/differentSymbolsNaive.py
@@ -0,0 +1,7 @@
+# Codesignal>Arcade>Intro #36 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/02/10
+# When I finish all Arcade, I can give a good answer.
+
+
+def differentSymbolsNaive(s):
+    return len(set(s))
