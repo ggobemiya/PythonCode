fe81422108d6f6f4605d7911efbb7387526787df
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-02-10 09:51:45 +0900

    Create firstDigit.py

---
+++ codesignal/intro/firstDigit.py
@@ -0,0 +1,9 @@
+# Codesignal>Arcade>Intro #35 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/02/10
+# When I finish all Arcade, I can give a good answer.
+
+
+def firstDigit(inputString):
+    for i in range(len(inputString)):
+        if inputString[i].isdigit():
+            return inputString[i]
