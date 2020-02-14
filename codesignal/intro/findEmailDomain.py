4b47c87e992c3cf5646b3f048f93df57a400d947
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-02-14 10:32:50 +0900

    Create findEmailDomain.py

---
+++ codesignal/intro/findEmailDomain.py
@@ -0,0 +1,6 @@
+# Codesignal>Arcade>Intro #44 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/02/14
+# When I finish all Arcade, I can give a good answer.
+
+def findEmailDomain(address):
+    return address[address.rfind("@")+1:]
