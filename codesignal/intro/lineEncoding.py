522afc4bf7874fb4f3a75e21ab5ebbb1244e36b2
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-02-26 10:59:52 +0900

    Update lineEncoding.py

--- codesignal/intro/lineEncoding.py
+++ codesignal/intro/lineEncoding.py
@@ -1,4 +1,4 @@
-# Codesignal>Arcade>Intro #48 Problem
+# Codesignal>Arcade>Intro #49 Problem
 # This is not a good answer. But that's the answer I can give you now. 20/02/26
 # When I finish all Arcade, I can give a good answer.
 
