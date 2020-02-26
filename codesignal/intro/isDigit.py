c0373f573c81876de9416f7ab85b5aa1ff42cc80
ggobemiya <46465631+ggobemiya@users.noreply.github.com>
2020-02-26 10:58:22 +0900

    Create isDigit.py

---
+++ codesignal/intro/isDigit.py
@@ -0,0 +1,6 @@
+# Codesignal>Arcade>Intro #48 Problem
+# This is not a good answer. But that's the answer I can give you now. 20/02/26
+# When I finish all Arcade, I can give a good answer.
+
+def isDigit(symbol):
+    return symbol.isdigit()
